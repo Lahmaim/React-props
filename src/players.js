c09025167340461d621820ae425b5d71830feaad
lahmaim <anas.lahmaim55@gmail.com>
2025-05-11 15:03:26 +0100

    ferst props in react

---
+++ src/players.js
@@ -0,0 +1,40 @@
+import bounou from "../src/assets/bounou.jpeg";
+import hakimi from "../src/assets/hakimi.jpeg";
+import rudiger from "../src/assets/rudiger.jpeg";
+import valverdi from "../src/assets/valverdi.jpeg";
+const players = [
+    {
+        name: "Yassine Bounou",
+        team: "Al Hilal",
+        nationality: "Morocco",
+        jerseyNumber: "13",
+        age: "34 Years old",
+        image: bounou,
+    },
+    {
+        name: "Achraf Hakimi",
+        team: "Paris Saint-Germain",
+        nationality: "Morocco",
+        jerseyNumber: "2",
+        age: "26 Years old",
+        image: hakimi,
+    },
+    {
+        name: "Antonio Rüdiger",
+        team: "Real Madrid",
+        nationality: " Germany",
+        jerseyNumber: "22",
+        age: "32 Years old",
+        image: rudiger,
+    },
+    {
+        name: "Federico Valverde",
+        team: "Real Madrid",
+        nationality: "Uruguay",
+        jerseyNumber: "15",
+        age: "26 Years old",
+        image: valverdi,
+    },
+];
+
+export default players;
